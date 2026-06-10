31511a464842056afdd7f0ea619128df0457bb0c
viviane.camara_undf <viviane.camara_undf@172.19.1.146>
2026-06-10 11:26:15 -0300

    commit de fim de aula

--- src/marketplace/Marketplace.java
+++ src/marketplace/Marketplace.java
@@ -11,13 +11,14 @@ public class Marketplace {
 		Scanner scanner = new Scanner(System.in);
 		
 		Servico servico1 = new Servico("lavanderia", "serviço para lavar roupas", 24);
-		servico1.listarServicos();
+		servico1.exibirInformações();
 		
 		Servico servico2 = new Servico("Marceneiro", "serviços de marcenaria" , 240);
-		servico2.listarServicos();
+		servico2.exibirInformações();
 		
 		Servico servico3 = new Servico("Eletricista", "Serviços de elétrica", 300);
-		servico3.listarServicos();
+		servico3.exibirInformações();
+		servico3.cadastrarNovoServico(scanner);
 		
 		scanner.close();
 	}
--- src/marketplace/Servico.java
+++ src/marketplace/Servico.java
@@ -1,11 +1,14 @@
 package marketplace;
 
+import java.util.Scanner;
+
 public class Servico {
 	//atributos
 	
 	String nome;
 	String descricao;
 	Float valor;
+	Scanner leitor;
 	
 	public Servico(String nome, String descricao, float valor) {
 		
@@ -14,14 +17,29 @@ public class Servico {
 		this.valor = valor;		
 	}
 	
-	public void cadastrarNovoServico() {
-		
-	}
-	
-	public void listarServicos() {
+	public void exibirInformações() {
 		System.out.println("Nome do serviço: " + nome);
 		System.out.println("Descrição do serviço: " + descricao);
 		System.out.println("Descrição do serviço: " + valor);
+	}
+	
+	public void cadastrarNovoServico(Scanner leitor) {
+		this.leitor = leitor;
+		
+		System.out.println("Digite o nome do nome serviço");
+		String nomeDigitado = leitor.next();
+		
+		System.out.println("Digite a descrição do serviço");
+		String descricaoDigitada = leitor.next();
+		
+		System.out.println("Digite o valor do serviço");
+		float valorDigitado = leitor.nextFloat();
+		
+		this.nome = nomeDigitado;
+		this.descricao = descricaoDigitada;
+		this.valor = valorDigitado;
+		
+		exibirInformações();
 	}
 
 }
