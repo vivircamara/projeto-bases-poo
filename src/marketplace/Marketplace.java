fbf34b2925a50cd1ee88e31b1498ed4824703ba0
viviane.camara_undf <viviane.camara_undf@172.19.1.113>
2026-06-17 09:14:23 -0300

    recriei os 3 serviços no Marketplace

--- src/marketplace/Marketplace.java
+++ src/marketplace/Marketplace.java
@@ -8,11 +8,15 @@ public class Marketplace {
 		Scanner scanner = new Scanner(System.in);
 		
 		Servico servico1 = new Servico("Criação de Logo", "Serviço para criação de logo.", 150);
+		
+		Servico servico2 = new Servico("Desenvolvimento de Site", "Serviço para desenvolvimento de site" , 2500); //como formatar para 2.500?
+		
+		Servico servico3 = new Servico("Consultoria em TI", "Serviço de consultoria em T.I", 400);
 			
-		Servico servico2 = new Servico();
+		Servico servico4 = new Servico();
 
 		Menu menu = new Menu();
-		menu.exibeOpcoes(servico2);
+		menu.exibeOpcoes(servico4);
 		
 		scanner.close();
 	}
